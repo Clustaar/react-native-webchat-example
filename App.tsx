cdeeda0bc2fe2fb7738f9920b4cde04d35750e5d
Zine Samy <zine.samy@gmail.com>
2020-03-11 00:52:08 +0100

    poc webchat with react ok

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React, { Component, useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, Dimensions } from 'react-native';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ClustaarWebChatService, WebChannel } from 'clustaar-webchat-sdk';
 import { InterlocutorReplyMessage } from 'clustaar-webchat-sdk/lib/domain/messages';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
-export default class App extends React.Component<{}, { message: string }> {
+export default class App extends React.Component<{}, { messages: any[] }> {
 
     botID = '5b50b57f64a5470032c98636';
     botToken = 'eyJ2YWx1ZSI6ImpncGVmcWh6S1BQUkJBdE1YSHRDYXFyLXJ5blFYU3B0QUZ2LVJFemoxQ00iLCJzdWJqZWN0Ijp7InR5cGUiOiJib3QiLCJpZCI6IjViNTBiNTdmNjRhNTQ3MDAzMmM5ODYzNiJ9fQ==';
@@ -18,41 +19,54 @@ export default class App extends React.Component<{}, { message: string }> {
     interlocutorChannel: WebChannel;
     interlocutorChannelSubject: Subject<any>;
     connectionState: string;
+    idMessage = 1;
+    width;
+    height;
+
 
     constructor(props) {
         super(props);
-
-        this.state = { message: '' };
+        const dimensions = Dimensions.get('window');
+        this.state = {
+            messages: []
+        };
         this.connect();
+        this.height = dimensions.height;
+        this.width = dimensions.width;
 
         this.clustaarWebchatSdkService.onConnectionState().subscribe((connectionState) => {
             this.connectionState = connectionState;
         });
         this.join();
+
     }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Text>React Native Clustaar Webchat SDK POC!</Text>
+            <View style={{ width: this.width, height: this.height }}>
                 <Text>State : {this.connectionState}</Text>
                 <TextInput style={{ height: 20, width: 150 }}
                            onChangeText={text => this.setState({ message: text })}
                            value={this.state.message}/>
-                <Button title="Send message" onPress={() => this.sendMessage()}/>
                 <Button title="Join" onPress={() => this.join()}/>
                 <Button title="Leave" onPress={() => this.leave()}/>
+                <GiftedChat
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
             </View>
         );
 
     }
 
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
 
-    connect() {
-        this.clustaarWebchatSdkService.connect();
-    }
-
-    sendMessage() {
         const interlocutorMessage: InterlocutorReplyMessage = {
             token: this.botToken,
             params: {
@@ -61,12 +75,17 @@ export default class App extends React.Component<{}, { message: string }> {
             },
             body: {
                 type: 'text',
-                message: this.state.message
+                message: messages[0].text
             }
         };
 
         this.interlocutorChannel.sendReply(interlocutorMessage);
-        this.setState({ message: '' });
+
+    }
+
+
+    connect() {
+        this.clustaarWebchatSdkService.connect();
     }
 
     join() {
@@ -81,20 +100,34 @@ export default class App extends React.Component<{}, { message: string }> {
 
         this.interlocutorChannel.join().subscribe((status) => {
             console.log(status);
-            this.interlocutorChannel.onBotReply().pipe(takeUntil(this.interlocutorChannelSubject)).subscribe((botReply) => {
-                console.log(botReply, 'botReply');
+            this.interlocutorChannel.onBotReply().pipe(takeUntil(this.interlocutorChannelSubject)).subscribe((botReply: any) => {
+                console.log('botReply:', botReply);
+                let textsAction = botReply.fulfillment.actions.filter(action => action.type == 'send_text_action');
+
+                for(let textAction of textsAction ) {
+                    this.addMessageToChat(textAction.text, 'AGENT')
+                }
             });
 
-            this.interlocutorChannel.onAgentReply().pipe(takeUntil(this.interlocutorChannelSubject)).subscribe((agentReply) => {
+            this.interlocutorChannel.onAgentReply().pipe(takeUntil(this.interlocutorChannelSubject)).subscribe((agentReply: any) => {
                 console.log(agentReply, 'agentReply');
+                this.addMessageToChat(agentReply.message, 'AGENT')
             });
 
             this.interlocutorChannel.onControl().pipe(takeUntil(this.interlocutorChannelSubject)).subscribe((control) => {
                 console.log(control, 'control');
+                if (control && control.value) {
+                    this.addMessageToChat('Un agent à rejoint la conversation', 'AGENT')
+                } else {
+                    this.addMessageToChat('Un agent à quitté la conversation', 'AGENT')
+                }
+
             });
 
             this.interlocutorChannel.onInterlocutorReply().pipe(takeUntil(this.interlocutorChannelSubject)).subscribe((interlocutorReply) => {
                 console.log(interlocutorReply, 'interlocutorReply');
+                this.addMessageToChat(interlocutorReply.message, 'USER')
+                // this.addMessageToChat('', 'USER')
             });
         });
     }
@@ -104,6 +137,25 @@ export default class App extends React.Component<{}, { message: string }> {
             this.interlocutorChannelSubject.next();
             this.interlocutorChannelSubject.complete();
         });
+    }
+
+    addMessageToChat(text, from) {
+        this.idMessage += 1;
+        const message = [{
+            _id: this.idMessage,
+            text: text,
+            createdAt: new Date(),
+            user: {
+                _id: from === 'USER' ? 1 : 2,
+                name: from,
+                avatar: 'https://placeimg.com/140/140/any',
+            },
+        }];
+
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, message)
+        }));
+
     }
 }
 
